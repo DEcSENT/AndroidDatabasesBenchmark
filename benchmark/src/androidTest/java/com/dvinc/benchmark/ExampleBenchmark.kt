da5e2199308982cc4959563416ffdea7499d8370
decsent@Yandex.ru <decsent@Yandex.ru>
2020-01-21 14:07:33 +0300

    Fixed all dependencies after migration to stable Benchmark version.

--- benchmark/src/androidTest/java/com/dvinc/benchmark/ExampleBenchmark.kt
+++ benchmark/src/androidTest/java/com/dvinc/benchmark/ExampleBenchmark.kt
@@ -1,10 +1,9 @@
 package com.dvinc.benchmark
 
 import android.util.Log
-import androidx.benchmark.BenchmarkRule
-import androidx.benchmark.measureRepeated
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.dvinc.database.TestClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
